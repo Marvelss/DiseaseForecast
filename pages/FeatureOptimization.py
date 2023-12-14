5e4f5117089a6aa13e4e48805497c8b3b9533d80
SakuraFox <982001940@qq.com>
2023-12-14 16:10:48 +0800

    特征提取的结果展示,参考JSON格式

--- pages/FeatureOptimization.py
+++ pages/FeatureOptimization.py
@@ -76,6 +76,12 @@ with dataPCR:
     tabb1, tabb2 = st.tabs(['结果', '可视化'])
     with tabb1:
         st.markdown('运行结果')
+        st.json(({
+            '气象数据': 'List (2个元素)',
+            '气温 List (2个特征)': [
+                '20.5',
+                '30.5',
+            ]}))
     with tabb2:
         st.subheader('展示数据处理前与处理后的图表')
         t1, t2 = st.columns(2)
