ad0539f04db95a6865aaf9394c70fb7389f6b0d0
SakuraFox <982001940@qq.com>
2024-01-10 16:03:20 +0800

    perf:调整了数据集上传和模板下载的布局,并在右侧添加了上传状态情况展示

--- pages/DataSet.py
+++ pages/DataSet.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
+import extra_streamlit_components as stx
 
 from pages_utils import multiselect_all
 
@@ -17,34 +18,70 @@ def convert_df(df):
 
 # st.header('数据集')
 # st.markdown('---')
-dataSCM, dataSCR = st.columns([0.5, 0.7])
+dataSCM, dataSCR = st.columns([0.9, 0.4])
+
 with dataSCM:
     st.markdown("##### 上传数据集")
-    tab1, tab2, tab3 = st.tabs(["气象数据", "植保数据", "农学数据"])
-    uploaded_files = st.file_uploader("上传数据集", accept_multiple_files=True, label_visibility='collapsed')
+    chosen_id = stx.tab_bar(data=[
+        stx.TabBarItemData(id=1, title="气象数据", description=""),
+        stx.TabBarItemData(id=2, title="植保数据", description=""),
+        stx.TabBarItemData(id=3, title="农学数据", description=""),
+    ], default=1)
+    uploaded_files = st.file_uploader(
+        "上传数据集",
+        accept_multiple_files=True,
+        label_visibility='collapsed',
+        type=['xlsx', 'csv', 'txt', 'xls'],
+        help='help', )
+    st.markdown('''
+        <style>
+            .uploadedFile {display: none}
+        <style>''',
+                unsafe_allow_html=True)
+    st.markdown('---')
+    st.markdown("###### 模板下载")
+    if chosen_id == '1':
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            option14 = st.checkbox('温度')
+        with col2:
+            option15 = st.checkbox('降水')
+        with col3:
+            option16 = st.checkbox('')
+    if chosen_id == '2':
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            option14 = st.checkbox('生育期')
+        with col2:
+            option15 = st.checkbox('模板6')
+        with col3:
+            option16 = st.checkbox('模板7')
+    if chosen_id == '3':
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            option14 = st.checkbox('生育期')
+        with col2:
+            option15 = st.checkbox('模板6')
+        with col3:
+            option16 = st.checkbox('模板7')
+    st.markdown('---')
+    st.markdown("###### 模板预览")
+    interval_col1, interval_col2 = st.columns([5, 1])
+    interval_col2.button('下载')
+with dataSCR:
+    st.markdown("##### 文件上传状态显示")
+    st.markdown("###### 气象数据")
+    st.write("文件名称:", '气温.xlsx')
+    st.markdown('---')
     for uploaded_file in uploaded_files:
         bytes_data = uploaded_file.read()
         st.write("filename:", uploaded_file.name)
-        st.write(bytes_data)
-
-    st.markdown("###### 模板下载")
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        option14 = st.checkbox('模板1')
-    with col2:
-        option15 = st.checkbox('模板2')
-    with col3:
-        option16 = st.checkbox('模板3')
-with dataSCR:
-    st.markdown("##### 模板预览")
+        dataframe = pd.read_excel(uploaded_file)
+        st.write(dataframe)
+    st.markdown("###### 植保数据")
+    col1ab, col2ab = st.columns(2)
+    col1ab.write("文件名称: 气温.xlsx")
+    col2ab.write("状态:已上传")
     st.markdown('---')
-    # tab3, tab4 = st.tabs(["", "其他"])
-    # with tab3:
-    csv = convert_df(pd.read_csv('resource/房价数据.csv'))
-    temperature = np.random.randint(low=0, high=40, size=1000)
-    df = pd.read_excel('resource/农学数据.xlsx', header=1)
-    st.data_editor(df)
-    interval_col1, interval_col2 = st.columns([5, 1])
-    interval_col2.button('下载')
-    # with tab4:
-    #     st.markdown('其他')
+    st.markdown("###### 农学数据")
+    st.write("文件名称:", '气温.xlsx')
