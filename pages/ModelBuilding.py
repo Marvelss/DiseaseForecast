64762e2dde898d58055f91331d3e67b7e1c461dd
SakuraFox <982001940@qq.com>
2024-01-10 17:02:32 +0800

    perf:调整了模型构建界面中建模方法、评价指标和数据集划分的展示效果
    通过使用st.empty和st.session_state实现了3种不同的内容切换,解决了使用单个st.empty切换导致的重复内容情况

--- pages/ModelBuilding.py
+++ pages/ModelBuilding.py
@@ -7,6 +7,16 @@ from streamlit_tree_select import tree_select
 # add_page_title()
 # st.header('模型构建')
 # st.markdown('---')
+# Pages logic
+if 'page' not in st.session_state: st.session_state.page = 0
+
+
+def nextPage(): st.session_state.page += 1
+
+
+def firstPage(): st.session_state.page = 0
+
+
 nodes = [
     {"label": "气象数据", "value": "folder_a"},
     {
@@ -103,41 +113,57 @@ with modelACV:
     st.markdown("###### 特征")
     temp2 = tree_select(nodes2)
 with modelACM:
-    st.markdown("###### 建模方法")
-    colOption1, colOption2, colOption3 = st.columns(3)
-    with colOption1:
-        agree = st.checkbox('SVM')
-        agree1 = st.checkbox('RF')
-    with colOption2:
-        agree2 = st.checkbox('KNN')
-        agree3 = st.checkbox('Logistic回归')
-    with colOption3:
-        agree4 = st.checkbox('贝叶斯统计')
-        agree5 = st.checkbox('模糊综合评价')
+    ph = st.empty()
 
-    if agree:
-        df = pd.DataFrame(
-            [
-                {"参数名": "a", "参数值": 4},
-                {"参数名": "b", "参数值": 4},
-                {"参数名": "c", "参数值": 6},
-                {"参数名": "d", "参数值": 5},
-            ]
-        )
-        st.data_editor(df)
-    st.markdown('---')
-    st.markdown("###### 评价指标")
-    agree6 = st.checkbox('OA')
-    agree7 = st.checkbox('Kappa')
-    st.markdown('---')
-    st.markdown("###### 验证与训练数据集划分")
-    option = st.selectbox(
-        label="划分比例",
-        options=("8:2", "7:3", "6:4")
-    )
-    # st.markdown("##### 模型参数设置")
-    interval_col1, interval_col2 = st.columns([2, 1])
-    interval_col2.button('开始模型训练')
+    # Page 0
+    if st.session_state.page == 0:
+        with ph.container():
+            st.markdown("###### 建模方法")
+            colOption1, colOption2, colOption3 = st.columns(3)
+            with colOption1:
+                agree = st.checkbox('SVM')
+                agree1 = st.checkbox('RF')
+            with colOption2:
+                agree2 = st.checkbox('KNN')
+                agree3 = st.checkbox('Logistic回归')
+            with colOption3:
+                agree4 = st.checkbox('贝叶斯统计')
+                agree5 = st.checkbox('模糊综合评价')
+            st.markdown('---')
+            if agree:
+                df = pd.DataFrame(
+                    [
+                        {"参数名": "a", "参数值": 4},
+                        {"参数名": "b", "参数值": 4},
+                        {"参数名": "c", "参数值": 6},
+                        {"参数名": "d", "参数值": 5},
+                    ]
+                )
+                st.data_editor(df)
+            interval_col1, interval_col2 = st.columns([4, 1])
+            interval_col2.button("下一步", on_click=nextPage)
+            # st.button()
+
+    # Page 1
+    elif st.session_state.page == 1:
+        with ph.container():
+            st.markdown("###### 评价指标")
+            agree6 = st.checkbox('OA')
+            agree7 = st.checkbox('Kappa')
+            interval_col1, interval_col2 = st.columns([4, 1])
+            interval_col2.button("下一步", on_click=nextPage)
+
+    # Page 2
+    elif st.session_state.page == 2:
+        with ph.container():
+            st.markdown("###### 验证与训练数据集划分")
+            option = st.selectbox(
+                label="划分比例",
+                options=("8:2", "7:3", "6:4")
+            )
+            interval_col1, interval_col2 = st.columns([4, 2])
+            interval_col1.button("返回", on_click=firstPage)
+            interval_col2.button("开始模型训练")
 with modelACR:
     tabb1, tabb2, tabb3 = st.tabs(['精度', '可视化', '工作区'])
 with tabb1:
