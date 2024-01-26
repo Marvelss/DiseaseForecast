63b03bd739697a209a4449cc4c7da033b283e8a1
SakuraFox <982001940@qq.com>
2024-01-26 13:40:14 +0800

    refactor:删除了多余注释代码

--- pages/DataPreparation.py
+++ pages/DataPreparation.py
@@ -9,17 +9,11 @@ import pages_utils
 from streamlit_autorefresh import st_autorefresh
 import seaborn as sns
 import matplotlib.pyplot as plt
-import extra_streamlit_components as stx
 
-data1 = [
-    stx.TabBarItemData(id=1, title="ToDo", description="Tasks to take care of"),
-    stx.TabBarItemData(id=2, title="Done", description="Tasks taken care of"),
-    stx.TabBarItemData(id=3, title="Overdue", description="Tasks missed out"),
-]
 if 'page12' not in st.session_state: st.session_state.page12 = 0
 if "leftTabs" not in st.session_state:
     # st.session_state["leftTabs"] = data1
-    st.session_state["leftTabs"] = ['标题1']
+    st.session_state["leftTabs"] = ['原始数据']
     # st.session_state["leftTabs"] = stx.tab_bar(data=[
     #     stx.TabBarItemData(id=1, title="气象数据", description=""),
     #     stx.TabBarItemData(id=2, title="植保数据", description=""),
@@ -86,19 +80,7 @@ def simulate_box_data():
 
 
 def nextPage():
-    # st.session_state["leftTabs"] = stx.tab_bar(data=[
-    #     stx.TabBarItemData(id=1, title="气象数据", description=""),
-    #     stx.TabBarItemData(id=2, title="植保数据", description=""),
-    #     stx.TabBarItemData(id=3, title="农学数据", description=""),
-    # ], default=1)
-    # new_tab = st.text_input("")
-    # st.session_state["leftTabs"].append("New Tab")
-    # # time.sleep(1)
-    # with leftDataTabs[len(st.session_state["leftTabs"])]:
-    #     st.markdown('s')
-    st.session_state["leftTabs"].append('标题2 ')
-
-    # st.experimental_rerun()
+    st.session_state["leftTabs"].append('预处理后数据')
     st.session_state.page12 += 1
     data11 = {"选择字段": True, "数据集": "气象数据", "字段": "温度",
               "大小": '1*3', "处理方法": "缺失值插补", "时间": '22:10:20',
@@ -112,194 +94,55 @@ def nextPage():
 
 if 'df11' not in st.session_state:
     st.session_state.df11 = pages_utils.PreprocessedDataSet
-# count = st_autorefresh(interval=2000, limit=100, key="fizzbuzzcounter")
 
-# The function returns a counter for number of refreshes. This allows the
-# ability to make special requests at different intervals based on the count
-# if count == 0:
-#     st.write("Count is zero")
-# nodes = [
-#     {"label": "气象数据", "value": "气象数据"},
-#     {
-#         "label": "植保数据",
-#         "value": "植保数据",
-#         "children": [
-#             {"label": "feature1", "value": "sub_a"},
-#             {"label": "feature2", "value": "sub_b"},
-#             {"label": "feature3", "value": "sub_c"},
-#         ],
-#     },
-#     {
-#         "label": "农学数据",
-#         "value": "folder_c",
-#         "children": [
-#             {"label": "晚稻移栽期", "value": "sub_d"},
-#             {
-#                 "label": "预测峰值",
-#                 "value": "sub_e",
-#                 "children": [
-#                     {"label": "测报站点", "value": "sub_sub_a"},
-#                     {"label": "生化指标", "value": "sub_sub_b"},
-#                 ],
-#             },
-#             {"label": "生化指标", "value": "sub_f"},
-#         ],
-#     },
-# ]
-#
-# nodes2 = [
-#     {"label": "气象数据", "value": "气象数据"},
-#     {
-#         "label": "植保数据",
-#         "value": "植保数据",
-#         "children": [
-#             {"label": "feature1", "value": "sub_4"},
-#             {"label": "feature2", "value": "sub_3"},
-#             {"label": "feature3", "value": "sub_2"},
-#         ],
-#     },
-#     {
-#         "label": "农学数据",
-#         "value": "folder_c",
-#         "children": [
-#             {"label": "晚稻移栽期", "value": "sub_d"},
-#             {
-#                 "label": "预测峰值",
-#                 "value": "sub_e",
-#                 "children": [
-#                     {"label": "测报站点", "value": "sub_sub_a"},
-#                     {"label": "生化指标", "value": "sub_sub_b"},
-#                 ],
-#             },
-#             {"label": "生化指标", "value": "sub_f"},
-#         ],
-#     },
-# ]
-#
-# st.session_state.page = 0
-
-
-# def addData(pha, rootNode):
-#     rootNode.append(
-#         {
-#             "label": "农学数据",
-#             "value": "folder_c",
-#             "children": [
-#                 {"label": "晚稻移栽期", "value": "sub_d"},
-#                 {
-#                     "label": "预测峰值",
-#                     "value": "sub_e",
-#                 }]})
-#     if st.session_state.page != -1:
-#         st.session_state.page += 1
-#         # st.write(st.session_state.page)
-#         # if st.session_state.page == 1:
-#         st.session_state.page += 1
-#         # st.write(st.session_state.page)
-#         with pha.container():
-#             st.markdown("sub_a" + str(st.session_state.page))
-#             # temp1 = tree_select(rootNode)
-
-
-# st.header('数据预处理')
-# st.markdown('---')
 # 界面名称+布局+布局内容
 # dataPreparation + column + variables
 dataPCV, dataPCM = st.columns([0.5, 0.7])
 with dataPCV:
     st.markdown("##### 数据与特征")
+    # 根据st.session_state.page12的值刷新表格
     placeholder1 = st.empty()
-    # leftDataTabs = st.tabs(st.session_state["leftTabs"])
-    st.markdown(st.session_state["leftTabs"])
     if st.session_state.page12 == 0:
         # st.markdown(st.session_state.page12)
         with placeholder1.container():
-            # if st.session_state.page12 == 1:
-            # st.markdown(st.session_state.page12)
-            st.tabs(st.session_state["leftTabs"])[0].markdown('标题1-1')
+            tt1 = st.tabs(st.session_state["leftTabs"])
+            for i in range(len(st.session_state["leftTabs"])):
+                with tt1[i]:
+                    st.data_editor(
+                        pages_utils.TempDataSet[i],
+                        height=720, width=800,
+                        column_config={
+                            "选择字段": st.column_config.CheckboxColumn(
+                                help="选择用于数据处理的字段",
+                                default=False,
+                            )
+                        })
 
     if st.session_state.page12 == 1:
         with placeholder1.container():
             tt = st.tabs(st.session_state["leftTabs"])
-            tt[0].markdown('标题1-2')
-            tt[1].markdown('标题2-1')
-        # with tab1:
-        #     st
-        # with tab2:
-        #     st.markdown('2')
-    # print(leftDataTabs)
-    # with leftDataTabs[0]:
-    #     # st.markdown("###### 原始数据")
-    #     # edited_df = st.data_editor(df)
-    #     edited_df22 = st.data_editor(
-    #         pages_utils.RawDataSet,
-    #         height=190, width=800,
-    #         column_config={
-    #             "选择字段": st.column_config.CheckboxColumn(
-    #                 help="选择用于数据处理的字段",
-    #                 default=False,
-    #             )
-    #         },
-    #         disabled=["数据集", "字段", "大小", "处理方法", "时间"],
-    #     )
-    # st.markdown('---')
-    # st.markdown("###### 预处理数据")
-    # edited_df223 = st.data_editor(
-    #     st.session_state.df11,
-    #     height=190, width=800,
-    #     column_config={
-    #         "选择字段": st.column_config.CheckboxColumn(
-    #             help="选择用于数据处理的字段",
-    #             default=False,
-    #         )
-    #     },
-    #     disabled=["数据集", "字段", "大小", "处理方法", "时间"])
-    # # for index, row in edited_df223.iterrows():
-    # #     st.markdown(row.get('下载数据集'))
-    # #     if row.get('下载数据集'):
-    # #         st.markdown('下载')
-    #
-    # st.markdown('---')
-    # st.markdown("###### 特征")
-    # edited_df2 = st.data_editor(
-    #     pages_utils.FeatureDataSet,
-    #     height=190, width=800,
-    #     column_config={
-    #         "选择字段": st.column_config.CheckboxColumn(
-    #             help="选择用于数据处理的字段",
-    #             default=False,
-    #         )
-    #     },
-    #     disabled=["数据集", "字段", "大小", "处理方法", "时间"], )
+            for i in range(len(st.session_state["leftTabs"])):
+                with tt[i]:
+                    st.data_editor(
+                        pages_utils.TempDataSet[i],
+                        height=720, width=800,
+                        column_config={
+                            "选择字段": st.column_config.CheckboxColumn(
+                                help="选择用于数据处理的字段",
+                                default=False,
+                            )
+                        })
 
-with (dataPCM):
-    # 当选择一类数据集后,其他数据集禁选
+with dataPCM:
     st.markdown("##### 预处理方法")
-
-    # dataFlag = temp.get('checked')
     # with tab1:
     col1, col2 = st.columns(2)
-    # st.dataframe(df.style.highlight_null(null_color='yellow'))
 
     with col1:
         agree = st.checkbox('剔除异常值', key='checkbox0', on_change=clear_other, args=[0])
         # agree5 = st.checkbox('剔除数据5')
     with col2:
         agree10 = st.checkbox("缺失值插补", key='checkbox1', on_change=clear_other, args=[1])
-    # st.markdown(dataFlag)
-    # if '植保数据' in dataFlag:
-    #     with col1:
-    #         agree111 = st.checkbox('剔除数据1')
-    #         # agree5 = st.checkbox('剔除数据5')
-    #     with col2:
-    #         agree101 = st.checkbox("标记不连续数据")
-    # with tab2:
-    # agree1 = st.checkbox('')
-    # # with tab3:
-    # agree4 = st.checkbox('剔除数据4')
-    # # with tab4:
-    # agree2 = st.checkbox('剔除数据2')
-    # agree3 = st.checkbox('剔除数据3')
     st.markdown('---')
     # st.markdown("##### 方法参数设置")
     if agree10:
@@ -338,8 +181,6 @@ with (dataPCM):
                 disabled=["数据集", "输入字段", "输出字段", "时间"], num_rows="dynamic", )
             interval_col34, interval_col33 = st.columns([5, 1])
             btn2 = interval_col33.button('运行', on_click=nextPage)
-            # st.session_state.page12 += 1
-        # if btn2:
 
     elif st.session_state.page12 == 1:
         with placeholder.container():
@@ -376,84 +217,3 @@ with (dataPCM):
                 # sns.(x='', y='', data=df, palette='Set3')
                 plt.title('Boxplot of Temperature for Different Days')
                 st.pyplot(fig)
-
-    # st.markdown(st.session_state.page12)
-    # st.rerun()
-# with dataPCR:
-#     tabb2, tabb3 = st.tabs(['可视化', ' '])
-# with tabb0:
-#     st.data_editor(pd.DataFrame(
-#         data={
-#             "温度": [1, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4],
-#         }
-#     ), height=210)
-#
-# with tabb1:
-#     st.markdown('##### 数据摘要')
-#     col111, col222 = st.columns(2)
-#     with col111:
-#         st.markdown('###### 处理前')
-#         st.markdown('特征名称:温度')
-#         st.markdown('个数:2')
-#         st.markdown('类型:整数')
-#         st.markdown('数值:[5,6]')
-#
-#     with col222:
-#         st.markdown('###### 处理后')
-#         st.markdown('特征名称:温度')
-#         st.markdown('个数:5')
-#         st.markdown('数据类型:整数')
-#         st.markdown('数值:[5,2,3,3,2]')
-# with tabb2:
-#     st.subheader('展示数据处理前与处理后的图表')
-#     t1, t2 = st.columns(2)
-#     with t1:
-#         st.image('resource/image/0.png')
-#     with t2:
-#         st.image('resource/image/1.png')
-# with tabb3:
-#     pass
-# st.markdown('###### 历史记录')
-# df = pd.DataFrame(
-#     {
-#         "数据集": ["气象数据", "植保数据", "农学数据"],
-#         "字段": ["温度", "生育期", "预测峰值"],
-#         "大小": ["1*3", "1*6", "1*5"],
-#         '处理方法': ['缺失值插补', '缺失值插补', '缺失值插补'],
-#         "时间": ['22:10:20', '20:10:20', '21:10:20'],
-#     }
-# )
-# edited_df = st.data_editor(df)
-# st.markdown('---')
-# st.markdown('###### 数据下载')
-# option = st.selectbox(
-#     '历史记录编号',
-#     (1, 2, 3))
-# pages_utils.multiselect_all(st, ['气温', '降雨日数', '生育期'], '字段', "collapsed")
-# interval_col13, interval_col12 = st.columns([5, 1])
-# interval_col12.button('下载')
-# with tab2:
-#     st.subheader('植保数据')
-# df = pd.read_excel('resource/植保数据 - 副本.xlsx')
-# agree1 = st.checkbox('标记不连续数据')
-# agree2 = st.checkbox('标记病株率负值数据', on_change=show)
-# t = st.dataframe(df.style.highlight_null(color='yellow'))
-# if agree2:
-#     t.empty()
-#     # 替换符合条件的单元格内容
-#     # df = df.replace('\\', np.nan)
-#     styled = df.style.applymap(highlight_negative, subset=['预测病株率'])
-#     if 'df' not in st.session_state:
-#         st.session_state.df = df
-#         st.session_state.styled_df = styled
-#     # ed = st.data_editor(st.session_state.styled_df)
-#
-#     st.dataframe(styled)
-# st.dataframe(df)
-
-# df1 = pd.read_excel('resource/植保数据 - 副本.xlsx')
-# # 替换符合条件的单元格内容
-# # df = df.replace('\\', np.nan)
-# styled = df1.style.applymap(highlight_negative, subset=pd.IndexSlice[:, ['预测病株率']])
-# edited_df = st.data_editor(styled)
-# edited_df=st.data_editor(styled,use_container_width=True,hide_index=True,disabled=["Host IP","Component","Validation_Command","Status"])
