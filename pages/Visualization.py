50e30f979e66038e78c2a5a56fe41af7f0883693
SakuraFox <982001940@qq.com>
2024-02-26 13:53:56 +0800

    pref:调整了页面布局,使其利用率更大

--- pages/Visualization.py
+++ pages/Visualization.py
@@ -11,30 +11,35 @@ import pages_utils
 st.set_page_config(
     layout="wide"
 )
-# with tab1:
-col1, col2 = st.columns(2)
 
-with col1:
-    st.markdown('##### 数据')
+# with tab1:
+tab1, tab2 = st.tabs(['数据', '可视化'])
+
+with tab1:
     tt1 = st.tabs(st.session_state["leftTabs"])
     for i in range(len(st.session_state["leftTabs"])):
         with tt1[i]:
             st.data_editor(
-                pages_utils.TempDataSet[i],
-                height=720, width=800)
-with col2:
-    st.markdown('##### 可视化')
-    option4 = st.selectbox(
-        '选择数据集',
-        options=st.session_state["leftTabs"])
-    option1 = st.selectbox(
-        '选择图形',
-        options=('散点图', '直方图'))
-    option2 = st.selectbox(
-        '选择X轴',
-        options=('年', 'DayOfYear'))
-    option3 = st.selectbox(
-        '选择Y轴',
-        options=('预测病株率', '病害发生程度'))
-    # with st.expander("选择坐标"):
-    st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+                pages_utils.TempDataSet[i])
+with tab2:
+    col1, col2 = st.columns([0.2, 0.7])
+    with col1:
+        option4 = st.selectbox(
+            '选择数据集',
+            options=st.session_state["leftTabs"])
+        option1 = st.selectbox(
+            '选择图形',
+            options=('散点图', '直方图'))
+        option2 = st.selectbox(
+            '选择X轴',
+            options=('年', 'DayOfYear'))
+        option3 = st.selectbox(
+            '选择Y轴',
+            options=('预测病株率', '病害发生程度'))
+        interval_col1, interval_col2 = st.columns([1.4, 1])
+        btn = interval_col2.button('添加图形')
+    with col2:
+        if btn:
+            st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+        else:
+            st.markdown('可视化')
