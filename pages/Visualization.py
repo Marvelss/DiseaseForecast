69a870f7bb4381eae393af2bc196db2486a3c442
SakuraFox <982001940@qq.com>
2024-02-26 10:17:48 +0800

    feat:添加了数据可视化界面

---
+++ pages/Visualization.py
@@ -0,0 +1,34 @@
+"""
+@Author : SakuraFox
+@Time: 2024-02-26 9:49
+@File : Visualization.py
+@Description : 数据可视化
+"""
+import streamlit as st
+
+import pages_utils
+
+# with tab1:
+col1, col2 = st.columns(2)
+
+with col1:
+    st.markdown('##### 数据')
+    tt1 = st.tabs(st.session_state["leftTabs"])
+    for i in range(len(st.session_state["leftTabs"])):
+        with tt1[i]:
+            st.data_editor(
+                pages_utils.TempDataSet[i],
+                height=720, width=800)
+with col2:
+    st.markdown('##### 可视化')
+    option1 = st.selectbox(
+        '选择图形',
+        options=('线性插值', '自定义'))
+    option2 = st.selectbox(
+        '选择X轴',
+        options=('年', 'DayOfYear'))
+    option3 = st.selectbox(
+        '选择Y轴',
+        options=('预测病株率', '病害发生程度'))
+    # with st.expander("选择坐标"):
+    st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
