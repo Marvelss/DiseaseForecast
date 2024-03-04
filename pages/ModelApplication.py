23352816040b7a03a0982ebc0f4fb6ef117e4d03
SakuraFox <982001940@qq.com>
2024-03-04 16:45:05 +0800

    pref:添加了页面小标题和说明文字图标

--- pages/ModelApplication.py
+++ pages/ModelApplication.py
@@ -30,6 +30,7 @@ def onRun(year, situation):
     st.toast('运行完成,数据准备完毕', icon='✅')
 
 
+st.markdown("##### 加载模型和特征")
 col2, col3 = st.columns(2)
 with col2:
     uploaded_model = st.file_uploader("加载模型")
@@ -45,7 +46,7 @@ with col3:
                 '* 1:高温多雨 2:高温常雨 3:高温少雨\n'
                 '* 4:常温常雨 5:常温多雨 6:常温少雨\n'
                 '* 7:低温少雨 8:低温常雨 9:低温多雨\n'
-                )
+                , icon="ℹ️")
         btn = st.button('运行程序', on_click=onRun, args=[float(generatedYears), float(weatherScenes)])
 
         if btn:
@@ -79,8 +80,9 @@ with col3:
 #     bytes_data = uploaded_file.read()
 #     st.write("filename:", uploaded_file.name)
 # st.write(bytes_data)
-
-st.button('运行')
-chart_data = pd.DataFrame(np.cumsum(np.random.randint(0, 2, size=(365, 1))), columns=["病株率(%)"])
-
-st.line_chart(chart_data)
+print('---')
+st.markdown("##### 可视化结果")
+btn = st.button('运行')
+if btn:
+    chart_data = pd.DataFrame(np.cumsum(np.random.randint(0, 2, size=(365, 1))), columns=["病株率(%)"])
+    st.line_chart(chart_data)
