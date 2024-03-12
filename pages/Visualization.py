a7d1358e458ddd8562b84adb5ee4abd3b41fcca7
SakuraFox <982001940@qq.com>
2024-03-12 09:16:15 +0800

    pref:添加了数据和模型下载UI

--- pages/Visualization.py
+++ pages/Visualization.py
@@ -13,15 +13,34 @@ st.set_page_config(
 )
 
 # with tab1:
-tab1, tab2 = st.tabs(['数据', '可视化'])
+tab1, tab2 = st.tabs(['数据及下载', '可视化'])
 
 with tab1:
-    tt1 = st.tabs(st.session_state["leftTabs"])
-    for i in range(len(st.session_state["leftTabs"])):
-        with tt1[i]:
-            st.data_editor(
-                pages_utils.TempDataSet[i],
-                height=800, width=1500)
+    col1, col2 = st.columns([0.2, 0.7])
+    with col1:
+        option55 = st.selectbox(
+            '选择下载内容',
+            options=st.session_state["leftTabs"])
+        if option55 == '模型':
+            result1 = pages_utils.multiselect_all(
+                st, '全选',
+                ['SVM', 'FLDA'],
+                'temp11', 'collapsed')
+            btn11 = st.button('下载特征和标签、模型结构及训练结果')
+        else:
+            result1 = pages_utils.multiselect_all(
+                st, '全选',
+                ['降水', '温度'],
+                'temp111', 'collapsed')
+            btn11 = st.button('下载')
+
+    with col2:
+        tt1 = st.tabs(st.session_state["leftTabs"])
+        for i in range(len(st.session_state["leftTabs"])):
+            with tt1[i]:
+                st.data_editor(
+                    pages_utils.TempDataSet[i],
+                    height=800, width=1500)
 with tab2:
     col1, col2 = st.columns([0.2, 0.7])
     with col1:
@@ -39,6 +58,7 @@ with tab2:
             options=('预测病株率', '病害发生程度'))
         interval_col1, interval_col2 = st.columns([1.4, 1])
         btn = interval_col2.button('添加图形')
+
     with col2:
         if btn:
             st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
