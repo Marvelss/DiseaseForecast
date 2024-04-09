bc0d11c06399fce8003b92c0dc1656c4d0410830
SakuraFox <982001940@qq.com>
2024-04-09 19:37:09 +0800

    pref:拆分模型应用和天气情景生成器

--- pages/App.py
+++ pages/App.py
@@ -25,6 +25,7 @@ show_pages(
         Page("pages/ModelBuilding.py", "模型构建"),
         Page("pages/ModelApplication.py", '模型应用'),
         Page("pages/Visualization.py", '可视化及数据下载'),
+        Page("pages/WeatherGenerator.py", '天气情景生成器'),
         Page("pages/ModelEvaluation.py", "测试界面")
     ]
 )
