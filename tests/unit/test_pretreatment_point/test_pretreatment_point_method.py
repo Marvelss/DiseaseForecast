b475c8f1855eb40ceed9240a42aade32bf8f15be
SakuraFox <982001940@qq.com>
2024-09-27 10:04:18 +0800

    test:整理测试用例

---
+++ tests/unit/test_pretreatment_point/test_pretreatment_point_method.py
@@ -0,0 +1,79 @@
+"""
+@Author : SakuraFox
+@Time: 2024-04-07 9:41
+@File : test_pretreatment_point_method.py
+@Description : 测试-预处理界面方法-点状
+"""
+import os
+import sys
+
+import allure
+import pytest
+
+# Add 'myproject' to sys.path based on the correct root directory
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+from myproject.pages.modelandmethod.PretreatmentMethod import PretreatmentMethod
+import unittest
+
+
+@allure.feature('单元测试(功能测试)')
+@allure.story('预处理界面 - 点状数据')
+# ===================测试处理后字段名称变化===================
+class PretreatmentPointMethod(unittest.TestCase):
+    """
+    测试点状 - 预处理界面
+    """
+
+    def setUp(self):
+        self.objPM = PretreatmentMethod(
+            None, None, None)
+
+    @allure.title("测试预处理前后字段名称变化-中文名称-首次处理")
+    @allure.severity(allure.severity_level.MINOR)
+    @allure.description('名称末尾添加预处理0')
+    def test_ChineseField(self):
+        self.assertEqual(
+            self.objPM.getHandledFieldPoint('温度'),
+            '温度-预处理后0')
+
+    @allure.title("测试预处理前后字段名称变化-英文名称-首次处理")
+    @allure.severity(allure.severity_level.MINOR)
+    @allure.description('名称末尾添加预处理0')
+    def test_EnglishField(self):
+        self.assertEqual(
+            self.objPM.getHandledFieldPoint('temperature'),
+            'temperature-预处理后0')
+
+    @allure.title("测试预处理前后字段名称变化-多次处理后")
+    @allure.severity(allure.severity_level.MINOR)
+    @allure.description('名称末尾添加的数字+1')
+    def test_MultiHandledField(self):
+        self.assertEqual(
+            self.objPM.getHandledFieldPoint('温度-预处理后3'),
+            '温度-预处理后4')
+
+    # ===================测试预处理方法===================
+    # 缺失值插补-自定义
+    @allure.title("测试预处理方法-自定义输入")
+    @allure.severity(allure.severity_level.MINOR)
+    @allure.description('自定义输入完成插补')
+    def test_MissingValueInterpolation_Custom(self):
+        self.objPM.dataFrame = ''
+        self.assertEqual(
+            self.objPM.getHandledFieldPoint('温度-预处理3'),
+            '温度-预处理4')
+
+    # 缺失值插补-线性插值
+    @allure.title("测试预处理方法-自动线性插补")
+    @allure.severity(allure.severity_level.MINOR)
+    @allure.description('基于pandas的线性插补')
+    def test_MissingValueInterpolation_LinearInterpolation(self):
+        self.assertEqual(
+            self.objPM.getHandledFieldPoint('温度-预处理3'),
+            '温度-预处理4')
+
+    # 异常值剔除
+
+
+if __name__ == '__main__':
+    unittest.main()
