bede9d668d4af4d969867b26be0d6a36cd927e04
SakuraFox <982001940@qq.com>
2024-12-16 19:09:13 +0800

    pref:面向植保部门大面积内容优化，准备公测6 气象数据预处理界面 添加异常值剔除及插补方法(可视化未添加) 1.具体数值与范围数值剔除并插补

--- myproject/pages/DataPreparation.py
+++ myproject/pages/DataPreparation.py
@@ -112,8 +112,10 @@ def detectUserInput():
 
 # 获取选项值对应名称
 def getCheckboxName(checkbox):
+    # if checkbox == 'checkbox0':
+    #     return '剔除异常值'
     if checkbox == 'checkbox0':
-        return '剔除异常值'
+        return '剔除异常值及插补'
     elif checkbox == 'checkbox1':
         return '缺失值插补'
 
@@ -199,7 +201,12 @@ def onRun():
                         st.toast(f'剔除异常值个数:{str(lengthBefore - lengthAfter)}' +
                                  '\n' +
                                  f'剩余条数:{lengthAfter}', icon='✅')
-
+                    elif tempMethod == '剔除异常值及插补':
+                        afterHandleData = PretreatmentMethod(
+                            dataFrameTemp,
+                            fields[indexT], reservedField).outlierEliminatorInterpolation(methodParam[indexT])
+                        # 显示填补信息
+                        st.toast(f'剔除异常值并插补完成', icon='✅')
                 except BaseException as e:
                     st.toast(f'{tempMethod}运行失败  \n错误:{e}', icon="⚠️")
                     # 不自动跳转至可视化
@@ -284,21 +291,21 @@ with dataPCV:
     # ===============显示左下字段或特征及获取===============
     # weatherNameList, plantNameList, agricultureNameList = ['无1'], ['无2'], ['无3']
     # if not pages_utils.TempDataSetField[0].empty:
-    weatherNameT1, plantNameT1, agricultureNameT1 = pages_utils.getDataFiled(0, pages_utils.TempDataSetField[0])
+    # weatherNameT1, plantNameT1, agricultureNameT1 = pages_utils.getDataFiled(0, pages_utils.TempDataSetField[0])
     # weatherNameList = weatherNameT1
     # plantNameList = plantNameT1
     # agricultureNameList = agricultureNameT1
     # if not pages_utils.TempDataSetField[1].empty:
-    weatherNameT2, plantNameT2, agricultureNameT2 = pages_utils.getDataFiled(1, pages_utils.TempDataSetField[1])
-    weatherNameList = weatherNameT1 + weatherNameT2
-    plantNameList = plantNameT1 + plantNameT2
-    agricultureNameList = agricultureNameT1 + agricultureNameT2
+    # weatherNameT2, plantNameT2, agricultureNameT2 = pages_utils.getDataFiled(1, pages_utils.TempDataSetField[1])
+    # weatherNameList = weatherNameT1 + weatherNameT2
+    # plantNameList = plantNameT1 + plantNameT2
+    # agricultureNameList = agricultureNameT1 + agricultureNameT2
     # 按照数据类型显示左侧字段或特征
     # result1 = pages_utils.multiselect_all(
     #     st, '全选-', filterUnique(weatherNameList, pages_utils.reservedField),
     #     'tempTemperature', 'collapsed')
     st.markdown("##### 气象数据字段选择")
-    result1 = pills("", filterUnique(weatherNameList, pages_utils.reservedField), label_visibility='collapsed')
+    result1 = pills("", filterUnique(pages_utils.TempDataSet[0], pages_utils.reservedField), label_visibility='collapsed')
     result1 = [result1]
     # result2 = pages_utils.multiselect_all(
     #     st, '全选-植保数据', filterUnique(plantNameList, pages_utils.reservedField),
@@ -312,12 +319,11 @@ with dataPCM:
     st.markdown("##### 预处理方法")
     # with tab1:
     col1, col2 = st.columns(2)
-
     with col1:
         agree = st.checkbox('剔除异常值及插补', key='checkbox0', on_change=clear_other, args=[0])
     with col2:
-        agree10 = st.checkbox("缺失值插补", key='checkbox1', on_change=clear_other, args=[1])
-
+        # agree10 = st.checkbox("缺失值插补", key='checkbox1', on_change=clear_other, args=[1])
+        agree10 = None
     # ===============显示和处理右中各个处理方法设置参数===============
     if agree10:
         # 显示缺失值信息
@@ -368,8 +374,6 @@ with dataPCM:
             st.image(img)
         # st.markdown('---')
     if agree:
-        st.markdown(' 1.异常值值内容(范围或具体值)  \n2.插补内容')
-
         # 异常值检测(温度和降水)
         # 显示缺失值信息
         # info = '疑似异常字段:\n'
@@ -381,38 +385,67 @@ with dataPCM:
 
         # infoT1 = PretreatmentMethod.detectLinearInterpolationWeather(dataFrameTemp)
         # infoT2 = PretreatmentMethod.detectLinearInterpolationRain(dataFrameTemp)
-        columnList, lowNum, upNum, lowCount, upCount = PretreatmentMethod.detect_outliers_iqr(dataFrameTemp,
-                                                                                              pages_utils.reservedField)
-        info1 = '无异常字段\n'
-        if not len(columnList):
+        # columnList, lowNum, upNum, lowCount, upCount = PretreatmentMethod.detect_outliers_iqr(dataFrameTemp,
+        #                                                                                       pages_utils.reservedField)
+        # info1 = '无异常字段\n'
+        # if not len(columnList):
+        #     st.info(f"{info1}\n", icon="ℹ️️")
+        # else:
+        #     st.info(f"{info1}\n", icon="ℹ️️")
 
-            st.info(f"{info1}\n", icon="ℹ️️")
-        else:
-            st.info(f"{info1}\n", icon="ℹ️️")
-
-            # infoT1 = ''
-            # for columnT, lowNumT, upNumT, lowCountT, upCountT in zip(columnList, lowNum, upNum, lowCount, upCount):
-            #     infoT1 += f"* {columnT} 下限:{round(lowNumT, 3)} 个数:{lowCountT} 上限:{round(upNumT, 3)} 个数:{upCountT}\n"
-            # st.warning(f"{info}  \n{infoT1}", icon="⚠️")
+        # infoT1 = ''
+        # for columnT, lowNumT, upNumT, lowCountT, upCountT in zip(columnList, lowNum, upNum, lowCount, upCount):
+        #     infoT1 += f"* {columnT} 下限:{round(lowNumT, 3)} 个数:{lowCountT} 上限:{round(upNumT, 3)} 个数:{upCountT}\n"
+        # st.warning(f"{info}  \n{infoT1}", icon="⚠️")
         st.markdown('---')
 
         coll11, coll22 = st.columns([0.3, 0.6])
         with coll11:
-            number2 = st.text_input("剔除大于以下数值外的值", value=0.1)
-            number3 = st.text_input("剔除小于以下数值外的值", value=0.1)
-            st.session_state["preMethodName"]['param1'] = number2
-            st.session_state["preMethodName"]['param2'] = number3
+            optionDP1 = st.selectbox(
+                '异常值查找方式',
+                ('具体数值', '范围数值'))
 
-            if number3:
-                # 检测剔除参数最小值>最大值
-                if PretreatmentMethod.detectLinearInterpolationParam([number2, number3]):
-                    st.toast('剔除数据的最小值>最大值', icon="⚠️")
+            if optionDP1 == '范围数值':
+                detectUp, detectLow = 0.1, 0.1
+                if '温度' == result1[0]:
+                    detectUp, detectLow = 50, -30
+                elif '降水' == result1[0]:
+                    detectUp, detectLow = 1500, 0
+                number2 = st.text_input("大于以下数值外的值", value=detectUp)
+                number3 = st.text_input("小于以下数值外的值", value=detectLow)
+                infoT1Low, infoT1Up = PretreatmentMethod.detectGeneralScope(dataFrameTemp, result1[0], float(number2),
+                                                                            float(number3))
+                if infoT1Low or infoT1Up:
+                    st.warning(f"存在异常数据:  \n超过上限数据:{infoT1Up}条  \n超过下限数据:{infoT1Low}条", icon="⚠️")
+                else:
+                    st.info(f"无异常数据\n", icon="ℹ️️")
+                option = st.selectbox(
+                    '插补方法',
+                    options='线性插值')
+                st.session_state["preMethodName"]['param1'] = optionDP1
+                st.session_state["preMethodName"]['param2'] = number2
+                st.session_state["preMethodName"]['param3'] = number3
+                if number3:
+                    # 检测剔除参数最小值>最大值
+                    if PretreatmentMethod.detectLinearInterpolationParam([number2, number3]):
+                        st.toast('剔除数据的最小值>最大值', icon="⚠️")
+            elif optionDP1 == '具体数值':
+                num = st.text_input('异常值', value=np.nan)
+                infoT1 = PretreatmentMethod.detectGeneralNumber(dataFrameTemp, result1[0], num)
+                if infoT1:
+                    st.warning(f"存在异常数据:{infoT1}条", icon="⚠️")
+                else:
+                    st.info(f"无异常数据\n", icon="ℹ️️")
+                num1 = st.text_input('插补值')
+                st.session_state["preMethodName"]['param1'] = optionDP1
+                st.session_state["preMethodName"]['param2'] = num
+                st.session_state["preMethodName"]['param3'] = num1
 
         with coll22:
             st.info('方法介绍\n'
                     '* 描述:剔除最大值和最小值区域外的异常值\n'
                     '* 疑似异常值检测:基于四分位数上下限\n', icon="ℹ️")
-            img = Image.open(os.path.join(RESOURCE_IMAGES_PATH, '3.png'))
+            img = Image.open(os.path.join(RESOURCE_IMAGES_PATH, 'Figure_5.png'))
             st.image(img)
 
     # =======================添加处理至任务清单=======================
@@ -580,6 +613,61 @@ with dataPCM:
                             # fig.suptitle(f'图{st.session_state.IMAGECOUNT} {data_before.name}数据剔除前后对比箱型图',
                             #              fontsize=16)
                             ax.set_xlabel('')
+                            st.pyplot(fig)
+                            st.session_state.IMAGECOUNT += 1
+
+                        elif idPreMethods[o] == '剔除异常值及插':
+                            data_before_temp = st.session_state["DPVisualInformation"][o]['before']
+                            data_after_temp = st.session_state["DPVisualInformation"][o]['after']
+                            # print(inputFields[o][0])
+                            data_before = pd.DataFrame({inputFields[o][0]: data_before_temp})
+                            data_after = pd.DataFrame({inputFields[o][0]: data_after_temp})
+                            # 查找缺失值的索引
+                            missing_indices = data_before[data_before[inputFields[o][0]].isna()].index
+                            # print(f'缺失索引:{missing_indices}')
+                            # 获取第一个缺失值的索引
+                            first_missing_index = missing_indices[0]
+                            # 获取第一个缺失值的行号
+                            # first_missing_index = 130
+                            # print(f"第一个缺失值的行号: {first_missing_index}")
+                            # 计算前5行和后5行的起始和结束索引
+                            start_index = max(first_missing_index - 80, 0)
+                            end_index = min(first_missing_index + 80 + 1, len(data_before))
+                            # 取第一个缺失值对应前15行和后15行预处理数据
+                            data_before_surrounding_data = data_before.iloc[start_index:end_index]
+                            data_after_surrounding_data = data_after.iloc[start_index:end_index]
+                            # 绘制对比折线图
+                            fig, ax = plt.subplots(figsize=(10, 6))
+                            # print(pages_utils.TempDataSet[1]['DayOfYear'])
+                            # 取第一个缺失值对应前15行和后15行'经度', '纬度', '年'数据
+                            missing_rows = \
+                                pages_utils.TempDataSet[1].loc[
+                                    missing_indices, ['经度', '纬度', '年', 'DayOfYear']].to_dict(
+                                    'records')[0]
+                            province, station, year = missing_rows['经度'], missing_rows['纬度'], missing_rows[
+                                '年']
+                            # print(missing_rows['DayOfYear'])
+                            # 整理前后15天dayOfYear为x轴
+                            # 提取数据列，获取从 start_index 到 end_index 范围内的行数
+                            # 生成从 start_index 到 end_index 的数字序列
+                            figure_x = pd.DataFrame({'row': list(range(start_index, end_index))})
+                            # 绘制插补前的折线图
+                            plt.plot(figure_x, data_before_surrounding_data[inputFields[o][0]],
+                                     label='原始数据',
+                                     color='black',
+                                     linestyle='-', marker='o')
+                            # 绘制插补后的折线图
+                            plt.plot(figure_x, data_after_surrounding_data[inputFields[o][0]],
+                                     label='插补后数据', color='blue',
+                                     linestyle='--',
+                                     marker='o', alpha=0.3)
+                            plt.xlabel('行号')
+                            ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+                            plt.ylabel(inputFields[o][0])
+                            plt.figtext(0.5, -0.01,
+                                        f'图{st.session_state.IMAGECOUNT} {inputFields[o][0]}字段部分数据插补前后对比图',
+                                        ha='center', fontsize=16)
+                            plt.legend()
                             st.pyplot(fig)
                             st.session_state.IMAGECOUNT += 1
 
--- myproject/pages/modelandmethod/PretreatmentMethod.py
+++ myproject/pages/modelandmethod/PretreatmentMethod.py
@@ -61,15 +61,36 @@ class PretreatmentMethod:
                 lower_outliers_list.append(len(lower_outliers))
                 upper_outliers_list.append(len(upper_outliers))
         return data.columns.tolist(), lower_bound_list, upper_bound_list, lower_outliers_list, upper_outliers_list
-        # 检测温度
 
+    # 检测通用-数值
+    @staticmethod
+    def detectGeneralNumber(dfT1, fieldTemp1, specific_value):
+        # 检查是否含有指定的值或NaN
+        if specific_value is not None:
+            if specific_value != 'nan':
+                specific_value = float(specific_value)
+                specific_value_count = dfT1[dfT1[fieldTemp1] == specific_value].shape[0]
+            else:
+                specific_value_count = len(dfT1[dfT1[fieldTemp1].isna()])
+        else:
+            specific_value_count = 0
+        return specific_value_count
+
+    # 检测通用-范围
+    @staticmethod
+    def detectGeneralScope(dfT1, fieldTemp1, upBound, lowBound):
+        lower_outliers = dfT1[dfT1[fieldTemp1] < lowBound]
+        upper_outliers = dfT1[dfT1[fieldTemp1] > upBound]
+        return len(lower_outliers), len(upper_outliers)
+
+    # 检测温度
     @staticmethod
     def detectLinearInterpolationWeather(dfT1):
         result = ''
         max_temp = dfT1['温度'].max()
         min_temp = dfT1['温度'].min()
         # 最高温度>45 最低温度<-15
-        if max_temp > 45:
+        if max_temp > 50:
             result += '温度最大值>45 '
         if min_temp < -15:
             result += '温度最大值<-15 '
@@ -146,3 +167,40 @@ class PretreatmentMethod:
         # 检查是否还有缺失值
         tempData = newDataFrame
         return tempData, lengthBefore, lengthAfter, newDataColumn
+
+    # 剔除异常值及插补
+    def outlierEliminatorInterpolation(self, methodParam):
+        print('-----------测试5------------')
+        print(methodParam)
+        # 处理单值或范围
+        flagSingle = methodParam[0]
+        # 处理单个字段
+        self.fieldName = self.fieldName[0]
+        # 复制新的变量
+        newDataFrame = self.dataFrame.copy()
+        newDataColumn = self.fieldName
+        if flagSingle == '具体数值':
+            missValue = methodParam[1]
+            filledValue = methodParam[2]
+            # 若指定字段为空
+            if missValue == 'nan':
+                missingValueBefore = (newDataFrame[newDataColumn] == np.nan).sum()
+                newDataFrame[newDataColumn] = newDataFrame[newDataColumn].fillna(float(filledValue))
+                missingValueAfter = (newDataFrame[newDataColumn] == np.nan).sum()
+            else:
+                missingValueBefore = (newDataFrame[newDataColumn] == float(missValue)).sum()
+                newDataFrame[newDataColumn] = newDataFrame[newDataColumn].replace(float(missValue), float(filledValue))
+                missingValueAfter = (newDataFrame[newDataColumn] == float(missValue)).sum()
+        elif flagSingle == '范围数值':
+            maxValue = float(methodParam[1])
+            minValue = float(methodParam[2])
+            # 剔除超出范围的异常值
+            df_filtered = newDataFrame[(newDataFrame[self.fieldName] >= minValue) &
+                                       (newDataFrame[self.fieldName] <= maxValue)]
+
+            # 使用线性插值填补NaN值
+            df_filtered[self.fieldName] = df_filtered[self.fieldName].interpolate(method='linear',
+                                                                                  limit_direction='both')
+            newDataFrame = df_filtered
+
+        return newDataFrame
