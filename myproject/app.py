aa4dd76cfc3209a0e6bb22ab1f4fd34322cacc12
SakuraFox <41704281+Marvelss@users.noreply.github.com>
2025-08-06 13:44:10 +0000

    chore: 修改系统介绍视频文件路径

--- myproject/app.py
+++ myproject/app.py
@@ -223,7 +223,7 @@ def emptyValue():
 
 
 category("✨ 系统介绍")
-video_file = open("./resource/videoIntroduction.mp4", "rb")
+video_file = open(os.path.join(RESOURCE_PATH, "videoIntroduction.mp4"), "rb")
 video_bytes = video_file.read()
 
 st.video(video_bytes)
